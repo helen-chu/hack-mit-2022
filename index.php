ad8cf3ae0862b2c403f2e5050b43d434e7327b1b
Helen Chu <55115447+helen-chu@users.noreply.github.com>
2022-10-01 20:22:03 -0400

    test

--- index.php
+++ index.php
@@ -1,40 +1,48 @@
-<!doctype html>
-<html>
-  <head>
-    <title>EnGauge</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-        <h1>EnGauge</h1><br>
-    <h3> Current Messages:</h3>
-
-    <div id="chatbox"><?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+ 
+        <title>Tuts+ Chat Application</title>
+        <meta name="description" content="Tuts+ Chat Application" />
+        <link rel="stylesheet" href="style.css" />
+    </head>
+    <body>
+    <?php
     session_start();
-        if(file_exists("log.html") && filesize("log.html") > 0){
-            
-            $contents = file_get_contents("log.html");         
-            echo $contents;
-        }
-    ?></div>
-
-    <form name="message" action="">
+    ?>
+        <div id="wrapper">
+            <div id="menu">
+                <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
+                <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
+            </div>
+ 
+            <div id="chatbox">
+            <?php
+            if(file_exists("log.html") && filesize("log.html") > 0){
+                $contents = file_get_contents("log.html");          
+                echo $contents;
+            }
+            ?>
+            </div>
+ 
+            <form name="message" action="">
                 <input name="usermsg" type="text" id="usermsg" />
                 <input name="submitmsg" type="submit" id="submitmsg" value="Send" />
             </form>
-
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        </div>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script type="text/javascript">
             // jQuery Document
             $(document).ready(function () {
-              //If user submits the form
-              $("#submitmsg").click(function () {
-                  var clientmsg = $("#usermsg").val();
-                  $.post("post.php", { text: clientmsg });
-                  $("#usermsg").val("");
-                  return false;
-              });
-
-              function loadLog() {
+                $("#submitmsg").click(function () {
+                    var clientmsg = $("#usermsg").val();
+                    $.post("post.php", { text: clientmsg });
+                    $("#usermsg").val("");
+                    return false;
+                });
+ 
+                function loadLog() {
                     var oldscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height before the request
  
                     $.ajax({
@@ -53,10 +61,14 @@
                 }
  
                 setInterval (loadLog, 2500);
-                
+ 
+                $("#exit").click(function () {
+                    var exit = confirm("Are you sure you want to end the session?");
+                    if (exit == true) {
+                    window.location = "index.php?logout=true";
+                    }
+                });
             });
         </script>
-  </body>
-
-
+    </body>
 </html>
