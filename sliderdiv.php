138d191be3dcf221e12dee2edb55c23f2c3ecf65
Helen Chu <55115447+helen-chu@users.noreply.github.com>
2022-10-02 02:26:06 -0400

    go

--- sliderdiv.php
+++ sliderdiv.php
@@ -3,78 +3,66 @@
     <head>
         <meta charset="utf-8" />
         <title>EnGauge</title>
+        <link href="sliderformat.css" rel="stylesheet">
         <link rel="stylesheet" href="style.css" />
     </head>
     
     <body>
         <?php
         session_start();
-        file_put_contents("log.html", 'testtttttt');
-
-        // if(isset($_GET['logout'])){    
-        
-        //     session_destroy();
-        //     header("Location: index.php"); //Redirect the user
-        // }
+        file_put_contents("slide.html", '');
         ?>
-        <div>
-		<input name="sliderval" type="range" min="0" max="100" value="50" id="sliderval">
-        </div>
-        <!-- <div>
-            <input name="submitval" type="submit" id="submitval" value="Send" />
-        </div> -->
-		<!-- <div id="selector">
-			<div class="SelectBtn"></div>
-			<div id="SelectValue"></div>
-		</div> -->
-
-        <div id="slidePlot">
-                <p>test</p>
-        <?php
-            if(file_exists("status.html") && filesize("status.html") > 0){
-                $contents = file_get_contents("status.html");          
+        <div id="wrapper">
+            <div id="chatbox">
+            <?php
+            if(file_exists("slide.html") && filesize("slide.html") > 0){
+                $contents = file_get_contents("slide.html");          
                 echo $contents;
             }
             ?>
+            </div>
+
         </div>
-
-            <!-- script to take in status bar input -->
+        <div id="slider">
+                <input type="range" min="0" max="100" value="50" id="bar">
+                <p>Value:<span id="value"></span></p>
+        </div>             
+        <div id="Submit">
+                <input type="submit" value="Submit" id="icon">
+        </div>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-		<script>
-                var slider = document.getElementById("sliderval"); 
-                var SelectValue = document.getElementById("SelectValue");
-                SelectValue.innerHTML = slider.value;
 
-                // jQuery Document
+	<script src="slider.js"></script>
+        <script type="text/javascript">
+            // jQuery Document
             $(document).ready(function () {
-                $("#sliderval").click(function () {
-                SelectValue.innerHTML = slider.value;
-                    var inputslide = $("#sliderval").val();
-                    $.post("slider.php", { slideval: SelectValue });
-                    $("#sliderval").val("");
+                $("#submit").click(function () {
+                    var clientmsg = $output.val();
+                    $.post("slider.php", { slideval: clientmsg });
+                    $("#usermsg").val("");
                     return false;
                 });
  
-                function loadSlide() {
-                    var oldscrollHeight = $("#slidePlot")[0].scrollHeight - 20; //Scroll height before the request
+                function loadLog() {
+                    var oldscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height before the request
  
                     $.ajax({
-                        url: "status.html",
+                        url: "slide.html",
                         cache: false,
                         success: function (html) {
-                            $("#slidePlot").html(html); //Insert slideplot log into the #slideplot div
+                            $("#chatbox").html(html); //Insert chat log into the #chatbox div
  
                             //Auto-scroll           
-                            var newscrollHeight = $("#slidePlot")[0].scrollHeight - 20; //Scroll height after the request
+                            var newscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height after the request
                             if(newscrollHeight > oldscrollHeight){
-                                $("#slidePlot").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+                                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
                             }   
                         }
                     });
                 }
-
-                setInterval (loadSlide, 2500);
-			})
-		</script>
+ 
+                setInterval (loadLog, 2500);
+            });
+        </script>
     </body>
 </html>
