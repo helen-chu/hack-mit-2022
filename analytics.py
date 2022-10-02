88872a2f8d0b46c97c2ddc96cab9500ba705a5e3
sushi <sushi@Catherines-MacBook-Pro-5.local>
2022-10-02 02:59:58 -0400

    t

--- analytics.py
+++ analytics.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+from scipy import interpolate
 
 try_data = {
     #every list: time, current score
@@ -19,12 +20,29 @@ def plot(data):
     num_vars = len(ys)
     for i in range(num_vars):
         plt.plot(x,ys[i])
-    #plt.legend()
+
+
+
+    # #make it smooth
+    # # x_new, bspline, y_new
+    # x_new = np.linspace(1, 5, 50)
+        
+    # for i in range(num_vars):
+    #     bspline = interpolate.make_interp_spline(x, ys[i])
+    #     y_new = bspline(x_new)
+
+    #     # Plot the new data points
+    #     plt.plot(x_new, y_new)
+
+    plt.xlabel("time(s)")
+    plt.ylabel("engagement level")
     plt.savefig("lines.png")
     plt.show()
 
-time_php = sys.argv[1]
-print(time_php)
+    
+
+#time_php = sys.argv[1]
+#print(time_php)
 
 
 plot(try_data)
