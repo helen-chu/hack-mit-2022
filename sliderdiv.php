5fd434bf7d8818a6940cc8f4562d0ec2cbac8161
Helen Chu <55115447+helen-chu@users.noreply.github.com>
2022-10-02 01:39:33 -0400

    attempt at recording slide bar

--- sliderdiv.php
+++ sliderdiv.php
@@ -23,10 +23,10 @@
         <!-- <div>
             <input name="submitval" type="submit" id="submitval" value="Send" />
         </div> -->
-		<div id="selector">
+		<!-- <div id="selector">
 			<div class="SelectBtn"></div>
 			<div id="SelectValue"></div>
-		</div>
+		</div> -->
 
         <div id="slidePlot">
         <?php
@@ -75,44 +75,5 @@
                 setInterval (loadSlide, 2500);
 			}
 		</script>
-	
-        <script type="text/javascript">
-            // jQuery Document
-            $(document).ready(function () {
-                $("#submitmsg").click(function () {
-                    var clientmsg = $("#usermsg").val();
-                    $.post("post.php", { text: clientmsg });
-                    $("#usermsg").val("");
-                    return false;
-                });
- 
-                function loadLog() {
-                    var oldscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height before the request
- 
-                    $.ajax({
-                        url: "log.html",
-                        cache: false,
-                        success: function (html) {
-                            $("#chatbox").html(html); //Insert chat log into the #chatbox div
- 
-                            //Auto-scroll           
-                            var newscrollHeight = $("#chatbox")[0].scrollHeight - 20; //Scroll height after the request
-                            if(newscrollHeight > oldscrollHeight){
-                                $("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-                            }   
-                        }
-                    });
-                }
- 
-                setInterval (loadLog, 2500);
- 
-                $("#exit").click(function () {
-                    var exit = confirm("Are you sure you want to end the session?");
-                    if (exit == true) {
-                    window.location = "index.php?logout=true";
-                    }
-                });
-            });
-        </script>
     </body>
 </html>
