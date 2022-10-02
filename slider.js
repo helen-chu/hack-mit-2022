fa134e794d43fb4ded9e9974e36e7ae7f403b328
James Pan <jamespan1118@icloud.com>
2022-10-02 01:09:53 -0400

    tried to print the value

---
+++ slider.js
@@ -0,0 +1,8 @@
+var slider = document.getElementById("bar");
+var output = document.getElementById("value");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
