3978b921cd89281233d280560d354606d09bfeae
Helen Chu <55115447+helen-chu@users.noreply.github.com>
2022-10-01 20:57:22 -0400

    kinda works?

--- index.php
+++ index.php
@@ -12,10 +12,6 @@
 
     if(isset($_GET['logout'])){    
      
-        //Simple exit message
-        $logout_message = "<div class='msgln'><span class='left-info'>User <b class='user-name-left'>". $_SESSION['name'] ."</b> has left the chat session.</span><br></div>";
-        file_put_contents("log.html", $logout_message, FILE_APPEND | LOCK_EX);
-         
         session_destroy();
         header("Location: index.php"); //Redirect the user
     }
