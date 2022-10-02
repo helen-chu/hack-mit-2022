9a6b11ea6317bba93465fcbb619b3562dc28e32d
Helen Chu <55115447+helen-chu@users.noreply.github.com>
2022-10-01 23:41:56 -0400

    attempt at recording slide bar

--- index.php
+++ index.php
@@ -136,9 +136,6 @@
 
             <!-- script to take in status bar input -->
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-        <script type="text/javascript">
-            
-        </script>
 
 		<div id="progressBar"></div>
 		<script>
@@ -154,34 +151,34 @@
 				selector.style.left = this.value + "%";
 				progressBar.style.width = (-this.value) + "%";
 
-                // jQuery Document
-            $(document).ready(function () {
-                $("#submitval").click(function () {
-                    var sliderval = $("#sliderval").val();
-                    $.post("slide.php", { slideval: slider.value });
-                    $("#usermsg").val("");
-                    return false;
-                });
+            //     // jQuery Document
+            // $(document).ready(function () {
+            //     $("#submitval").click(function () {
+            //         var sliderval = $("#sliderval").val();
+            //         $.post("slide.php", { slideval: slider.value });
+            //         $("#usermsg").val("");
+            //         return false;
+            //     });
  
-                function loadSlide() {
-                    var oldscrollHeight = $("#slidePlot")[0].scrollHeight - 20; //Scroll height before the request
+            //     function loadSlide() {
+            //         var oldscrollHeight = $("#slidePlot")[0].scrollHeight - 20; //Scroll height before the request
  
-                    $.ajax({
-                        url: "status.html",
-                        cache: false,
-                        success: function (html) {
-                            $("#slidePlot").html(html); //Insert slideplot log into the #slideplot div
+            //         $.ajax({
+            //             url: "status.html",
+            //             cache: false,
+            //             success: function (html) {
+            //                 $("#slidePlot").html(html); //Insert slideplot log into the #slideplot div
  
-                            //Auto-scroll           
-                            var newscrollHeight = $("#slidePlot")[0].scrollHeight - 20; //Scroll height after the request
-                            if(newscrollHeight > oldscrollHeight){
-                                $("#slidePlot").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-                            }   
-                        }
-                    });
-                }
+            //                 //Auto-scroll           
+            //                 var newscrollHeight = $("#slidePlot")[0].scrollHeight - 20; //Scroll height after the request
+            //                 if(newscrollHeight > oldscrollHeight){
+            //                     $("#slidePlot").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
+            //                 }   
+            //             }
+            //         });
+            //     }
 
-                setInterval (loadSlide, 2500);
+            //     setInterval (loadSlide, 2500);
 			}
 		</script>
 	</div>
